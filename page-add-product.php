83ecb468cdb0ef76eb8d627f23af608c1f1c1eeb
PhemcodeJay <phemcodejay@gmail.com>
2024-08-07 20:52:06 +0300

    up

--- page-add-product.php
+++ page-add-product.php
@@ -31,7 +31,6 @@ if (!$user_info) {
 $email = htmlspecialchars($user_info['email']);
 $date = htmlspecialchars($user_info['date']);
 
-// Ensure this PHP script is accessed through a POST request
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try {
         // Check if the user is logged in
@@ -115,7 +114,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Execute the statement and check for success
         if ($stmt->execute()) {
             // Redirect back to listing page after insertion
-            header('Location: page-list-product.php');
             exit();
         } else {
             // Log the error if insertion failed
@@ -139,8 +137,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 
-<!doctype php>
-<php lang="en">
+<!doctype html>
+<html lang="en">
   <head>
     <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -165,7 +163,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       
       <div class="iq-sidebar  sidebar-default ">
           <div class="iq-sidebar-logo d-flex align-items-center justify-content-between">
-              <a href="http://localhost/project/dashboard.php" class="header-logo">
+              <a href="http://localhost/project/dashboard.html" class="header-logo">
                   <img src="http://localhost/project/assets/images/logo.png" class="img-fluid rounded-normal light-logo" alt="logo"><h5 class="logo-title light-logo ml-3">SalesPilot</h5>
               </a>
               <div class="iq-menu-bt-sidebar ml-0">
@@ -176,7 +174,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
               <nav class="iq-sidebar-menu">
                   <ul id="iq-sidebar-toggle" class="iq-menu">
                       <li class="">
-                          <a href="http://localhost/project/dashboard.php" class="svg-icon">                        
+                          <a href="http://localhost/project/dashboard.html" class="svg-icon">                        
                               <svg  class="svg-icon" id="p-dash1" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                   <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line>
                               </svg>
@@ -195,12 +193,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                           </a>
                           <ul id="product" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
                               <li class="">
-                                  <a href="http://localhost/project/page-list-product.php">
+                                  <a href="http://localhost/project/page-list-product.html">
                                       <i class="las la-minus"></i><span>List Product</span>
                                   </a>
                               </li>
                               <li class="active">
-                                  <a href="http://localhost/project/page-add-product.php">
+                                  <a href="http://localhost/project/page-add-product.html">
                                       <i class="las la-minus"></i><span>Add Product</span>
                                   </a>
                               </li>
@@ -218,7 +216,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                           </a>
                           <ul id="category" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
                                   <li class="">
-                                          <a href="http://localhost/project/page-list-category.php">
+                                          <a href="http://localhost/project/page-list-category.html">
                                               <i class="las la-minus"></i><span>List Category</span>
                                           </a>
                                   </li>
@@ -237,12 +235,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                           </a>
                           <ul id="sale" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
                                   <li class="">
-                                          <a href="http://localhost/project/page-list-sale.php">
+                                          <a href="http://localhost/project/page-list-sale.html">
                                               <i class="las la-minus"></i><span>List Sale</span>
                                           </a>
                                   </li>
                                   <li class="">
-                                          <a href="http://localhost/project/page-add-sale.php">
+                                          <a href="http://localhost/project/page-add-sale.html">
                                               <i class="las la-minus"></i><span>Add Sale</span>
                                           </a>
                                   </li>
@@ -260,12 +258,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                           </a>
                           <ul id="purchase" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
                                   <li class="">
-                                          <a href="http://localhost/project/page-list-expense.php">
+                                          <a href="http://localhost/project/page-list-expense.html">
                                               <i class="las la-minus"></i><span>List Expenses</span>
                                           </a>
                                   </li>
                                   <li class="">
-                                          <a href="http://localhost/project/page-add-expense.php">
+                                          <a href="http://localhost/project/page-add-expense.html">
                                               <i class="las la-minus"></i><span>Add Expenses</span>
                                           </a>
                                   </li>
@@ -283,7 +281,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                           </a>
                           <ul id="return" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
                                   <li class="">
-                                          <a href="http://localhost/project/page-list-inventory.php">
+                                          <a href="http://localhost/project/page-list-inventory.html">
                                               <i class="las la-minus"></i><span>List Inventory</span>
                                           </a>
                                   </li>
@@ -302,39 +300,39 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                           </a>
                           <ul id="people" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
                                   <li class="">
-                                          <a href="http://localhost/project/page-list-customers.php">
+                                          <a href="http://localhost/project/page-list-customers.html">
                                               <i class="las la-minus"></i><span>Customers</span>
                                           </a>
                                   </li>
                                   <li class="">
-                                          <a href="http://localhost/project/page-add-customers.php">
+                                          <a href="http://localhost/project/page-add-customers.html">
                                               <i class="las la-minus"></i><span>Add Customers</span>
                                           </a>
                                   </li>
                                   <li class="">
-                                          <a href="http://localhost/project/backend/page-list-staffs.php">
+                                          <a href="http://localhost/project/backend/page-list-staffs.html">
                                               <i class="las la-minus"></i><span>Staffs</span>
                                           </a>
                                   </li>
                                   <li class="">
-                                          <a href="http://localhost/project/page-add-staffs.php">
+                                          <a href="http://localhost/project/page-add-staffs.html">
                                               <i class="las la-minus"></i><span>Add Staffs</span>
                                           </a>
                                   </li>
                                   <li class="">
-                                          <a href="http://localhost/project/page-list-suppliers.php">
+                                          <a href="http://localhost/project/page-list-suppliers.html">
                                               <i class="las la-minus"></i><span>Suppliers</span>
                                           </a>
                                   </li>
                                   <li class="">
-                                          <a href="http://localhost/project/page-add-supplier.php">
+                                          <a href="http://localhost/project/page-add-supplier.html">
                                               <i class="las la-minus"></i><span>Add Suppliers</span>
                                           </a>
                                   </li>
                           </ul>
                       </li>
                       <li class="">
-                          <a href="http://localhost/project/analytics.php" class="">
+                          <a href="http://localhost/project/analytics.html" class="">
                               <svg class="svg-icon" id="p-dash7" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                   <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline>
                               </svg>
@@ -342,7 +340,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                           </a>
                           <ul id="reports" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
                           </ul>
-                          <a href="http://localhost/project/table-data.php" class="">
+                          <a href="http://localhost/project/table-data.html" class="">
                               <svg class="svg-icon" id="p-dash7" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                   <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline>
                               </svg>
@@ -357,7 +355,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
               <nav class="navbar navbar-expand-lg navbar-light p-0">
                   <div class="iq-navbar-logo d-flex align-items-center justify-content-between">
                       <i class="ri-menu-line wrapper-menu"></i>
-                      <a href="http://localhost/project/dashboard.php" class="header-logo">
+                      <a href="http://localhost/project/dashboard.html" class="header-logo">
                           <img src="http://localhost/project/assets/images/logo.png" class="img-fluid rounded-normal" alt="logo">
                           <h5 class="logo-title ml-3">SalesPilot</h5>
       
@@ -584,11 +582,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                                       class="rounded profile-img img-fluid avatar-70">
                                               </div>
                                               <div class="p-3">
-                                                  <h5 class="mb-1"><?php echo $email; ?></h5>
-                                                    <p class="mb-0">Since <?php echo $date; ?></p>
+                                                  <h5 class="mb-1"><?html echo $email; ?></h5>
+                                                    <p class="mb-0">Since <?html echo $date; ?></p>
                                                   <div class="d-flex align-items-center justify-content-center mt-3">
-                                                      <a href="http://localhost/project/user-profile-edit.php" class="btn border mr-2">Profile</a>
-                                                      <a href="logout.php" class="btn border">Sign Out</a>
+                                                      <a href="http://localhost/project/user-profile-edit.html" class="btn border mr-2">Profile</a>
+                                                      <a href="logout.html" class="btn border">Sign Out</a>
                                                   </div>
                                               </div>
                                           </div>
@@ -634,7 +632,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     </div>
                 </div>
                 <div class="card-body">
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data" data-toggle="validator">
+                    <form action="<?php echo htmlspecialchars($_SERVER["html_SELF"]); ?>" method="POST" enctype="multipart/form-data" data-toggle="validator">
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group">
@@ -746,12 +744,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <div class="row">
                         <div class="col-lg-6">
                             <ul class="list-inline mb-0">
-                                <li class="list-inline-item"><a href="http://localhost/project/privacy-policy.php">Privacy Policy</a></li>
-                                <li class="list-inline-item"><a href="http://localhost/project/terms-of-service.php">Terms of Use</a></li>
+                                <li class="list-inline-item"><a href="http://localhost/project/privacy-policy.html">Privacy Policy</a></li>
+                                <li class="list-inline-item"><a href="http://localhost/project/terms-of-service.html">Terms of Use</a></li>
                             </ul>
                         </div>
                         <div class="col-lg-6 text-right">
-                            <span class="mr-1"><script>document.write(new Date().getFullYear())</script>©</span> <a href="http://localhost/project/dashboard.php" class="">SalesPilot</a>.
+                            <span class="mr-1"><script>document.write(new Date().getFullYear())</script>©</span> <a href="http://localhost/project/dashboard.html" class="">SalesPilot</a>.
                         </div>
                     </div>
                 </div>
@@ -788,4 +786,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </script>
     
   </body>
-</php>
+</html>
