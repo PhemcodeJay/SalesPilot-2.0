cd1a41818f08ac0a21022cd808a69ba107025d23
PhemcodeJay <phemcodejay@gmail.com>
2024-09-05 23:36:51 +0300

    up

--- page-add-sale.php
+++ page-add-sale.php
@@ -560,7 +560,7 @@ try {
                                               <h5 class="mb-1"><?php echo $email; ?></h5>
                                               <p class="mb-0">Since <?php echo $date; ?></p>
                                                   <div class="d-flex align-items-center justify-content-center mt-3">
-                                                      <a href="http://localhost/project/app/user-profile.php" class="btn border mr-2">Profile</a>
+                                                      <a href="http://localhost/project/user-profile-edit.php" class="btn border mr-2">Profile</a>
                                                       <a href="logout.php" class="btn border">Sign Out</a>
                                                   </div>
                                               </div>
